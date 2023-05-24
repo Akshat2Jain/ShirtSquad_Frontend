03b173c344b1af92bc93ee0a5c6e7a535e46787f
Akshat Jain <101265586+Akshat2Jain@users.noreply.github.com>
2023-05-24 16:43:44 +0530

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -8,10 +8,10 @@ const Success = () => {
             <Wrapper>
                 <div className="max-w-[600px] rounded-lg p-5 border border-black mx-auto flex flex-col">
                     <div className="text-2xl font-bold">
-                        If You are Esha
+                        Thanks for shopping with us!
                     </div>
                     <div className="text-lg font-bold mt-2">
-                        Then I love you ❤️
+                        Your order has been placed successfully.
                     </div>
                     <div className="text-base mt-5">
                         For any product related query, drop an email to
